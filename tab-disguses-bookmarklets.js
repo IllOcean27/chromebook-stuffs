a2a32c7ceadc2e87e4f151fa4be13ecf5492267a
IllOcean27 <121472567+IllOcean27@users.noreply.github.com>
2023-04-05 11:04:13 -0600

    Create tab-disguses-bookmarklets.js

---
+++ tab-disguses-bookmarklets.js
@@ -0,0 +1,14 @@
+//_______________________________________________________________________________//
+//                            ****Tab Disgises****                              //
+//                                                                             //
+// Use these tab disguses to hide any tab. More will be added by request.     //
+// To request one, just DM me on Discord, IllOcean27#4588.                   //
+// _________________________________________________________________________//
+
+// Google Docs
+
+javascript:(function() {var link = document.querySelector("link[rel*='icon']") || document.createElement('link');link.type = 'image/x-icon';link.rel = 'shortcut icon';link.href = 'https://cdn4.iconfinder.com/data/icons/free-colorful-icons/360/google_docs.png';document.title = 'Google Docs';console.log(document.title);document.getElementsByTagName('head')[0].appendChild(link);})();
+
+// Clever Portal
+
+javascript:(function() {var link = document.querySelector("link[rel*='icon']") || document.createElement('link');link.type = 'image/x-icon';link.rel = 'shortcut icon';link.href = 'https://assets.clever.com/launchpad/b2a286f20/favicon.ico?1%27;document.title = %27Clever | Portal%27;console.log(document.title);document.getElementsByTagName(%27head%27)[0].appendChild(link);})();
